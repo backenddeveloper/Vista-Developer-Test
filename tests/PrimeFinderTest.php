8d6584ff2a972b40b891e6df4e334ca33a1a7050
Graham Turner <grahamturnernew@hotmail.com>
2015-08-16 10:10:01 +0100

    The first test has been built.

---
+++ tests/PrimeFinderTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class PrimeFinderTest extends PHPUnit_Framework_TestCase {
+	
+	public $test ;
+	
+	public function setup(){
+		
+		$this->test = new PrimeFinder(10) ;
+		
+	}
+	
+	public function testList(){
+		
+		$list = $this->test->getListOfPrimeNumbers() ;
+		
+		$this->assertEquals($list,Array(2,3,5,7,11,13,17,19,23,29));
+		
+	}
+	
+}
