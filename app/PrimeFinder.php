1e07e79d716cc4d2263123781d9f85d4b5f47905
Graham Turner <grahamturnernew@hotmail.com>
2015-08-16 11:59:42 +0100

    Passed the second test, now reliably considered working.

--- app/PrimeFinder.php
+++ app/PrimeFinder.php
@@ -4,11 +4,63 @@ namespace ForVista ;
 
 final class PrimeFinder {
 	
-	public function __construct($numberOfPrimesRequired){}
+	private $numberOfPrimesRequired ;
+	
+	private $listOfPrimeNumbers = Array() ;	
+	
+	public function __construct($numberOfPrimesRequired){
+		
+		if(is_int($numberOfPrimesRequired) && $numberOfPrimesRequired > 0){
+			
+			$this->numberOfPrimesRequired = $numberOfPrimesRequired ;
+
+		} else {
+			
+			throw new \InvalidArgumentException("A non-valid input was entered, expected an integer") ;
+			
+		}
+		
+		$this->buildListOfPrimeNumbers() ;
+		
+	}
+	
+	private function buildListOfPrimeNumbers(){
+		
+		$numberToTest = 2 ; //The number 'one' is not defined as a prime number.
+
+		while(count($this->listOfPrimeNumbers) < $this->numberOfPrimesRequired){
+			
+			if($this->isPrime($numberToTest)){
+
+				array_push($this->listOfPrimeNumbers, $numberToTest) ;
+				
+			}
+			
+			$numberToTest++ ;
+			
+		}				
+
+	}
+	
+	private function isPrime($numberToTest){
+		
+		for($divisor = 2 ; $divisor <= sqrt($numberToTest) ; $divisor++){
+			
+			if($numberToTest % $divisor == 0){
+				
+				return false ;
+				
+			}
+			
+		}
+		
+		return true ;
+		
+	}
 	
 	public Function getListOfPrimeNumbers(){
 		
-		return Array(2,3,5,7,11,13,17,19,23,29) ;
+		return $this->listOfPrimeNumbers ;
 		
 	}
 	
